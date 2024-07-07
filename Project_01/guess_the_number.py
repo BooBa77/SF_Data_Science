cf436d8061f9d22069a62aae084caac53c091f66
Sergey Tregubov <booba@yandex.ru>
2024-07-07 17:53:01 +0800

    Проект 01. Исправлен код на загадывание числа компьютером. Оформлены README.MD, добавлен requirements.txt

--- Project_01/guess_the_number.py
+++ Project_01/guess_the_number.py
@@ -1,22 +1,36 @@
 def guess_my_num(number: int = 1) -> int:
     """
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Загаданное число
 
     Returns:
         int: Число попыток, затраченное на угадывание
     """
-    count = 1
-    predict = 50
+    count = 1 # кол-во попыток
+    predict = 50 # предполагаемое число
+
+    """
+    Угадывать число будем, пользуясь подсказкой "больше/меьше", 
+    начиная с середины, т.е. 50,
+    смещаясь в предполжении на половину оставшего диапозона.
+    И он, с свою очередь, уменьшается с каждой попыткой в два раза,
+    или по формуле 100 / 2^count
+    """
 
     while number != predict:
         count += 1
         if number > predict:
-            predict = predict + int((100-1) / (2**count)) + 1
+            predict = predict + int(100 / (2**count)) + 1
         elif number < predict:
-            predict = predict - int((100-1) / (2**count)) - 1
+            predict = predict - int(100 / (2**count)) - 1
 
     return count
     
-for i in range(101):
-    print(f'{i} отгадано с ' + str(game_core_v3(i)) + ' попыток')
+import numpy as np
+count_attempts = 0 # общее кол-во попыток отгадывания
+for i in range(10000):
+    hidden_number = np.random.randint(0, 101)  # загаданное компьютером число
+    count_attempts += guess_my_num(hidden_number)
+
+# вывод на экран среднего количества попыток
+print(f'10000 чисел отгадано в среднем за ' + str(round(count_attempts / 10000, 2)) + ' попыток')
